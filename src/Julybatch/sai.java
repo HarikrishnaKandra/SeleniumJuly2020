541063b67baaf611b156046f743299a92cc5bbb5
munis <munis@LAPTOP-BFD39EFV>
2020-07-28 11:11:22 +0530

    changed the driver

--- src/Julybatch/sai.java
+++ src/Julybatch/sai.java
@@ -8,7 +8,7 @@ public class sai {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("weddriver.chrome.driver","C:\\Users\\munis\\Desktop\\SELENIUM\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\munis\\Desktop\\SELENIUM\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.gmail.com");
 System.out.println("read");
